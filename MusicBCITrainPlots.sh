045501954975510bbb52c89fa46767161a93e51a
BenR <90284651+AsianUser@users.noreply.github.com>
2026-04-22 17:53:46 -0700

    new job for plot making

---
+++ MusicBCITrainPlots.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#SBATCH --job-name=MusicBCI_train
+#SBATCH --output=sbatch_outputs/MusicBCI_%j.out
+#SBATCH --error=sbatch_outputs/MusicBCI_%j.err
+#SBATCH --time=4:00:00
+#SBATCH --mem=16G
+#SBATCH --cpus-per-task=2
+
+module purge
+module load conda
+
+# Initialize conda
+source /apps/conda/miniforge3/24.11.3/etc/profile.d/conda.sh
+
+conda activate musicbci
+
+# Run training script
+python -u confusion_matrix.py
+
+echo "===== JOB END ====="
+
+conda deactivate
