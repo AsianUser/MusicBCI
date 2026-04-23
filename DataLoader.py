285bf4c9bbd28a2799b905413d3cfb5f2fed0f8c
BenR <90284651+AsianUser@users.noreply.github.com>
2026-04-23 16:15:26 -0700

    debug

--- DataLoader.py
+++ DataLoader.py
@@ -98,7 +98,10 @@ def make_per_trigger_dataloaders(
     X_all: torch.Tensor = meta["X"]  # [N, channels, time]
     y_all: torch.Tensor = meta["y"]  # [N]
 
-    unique_labels = sorted(y_all.unique().tolist())
+    # unique_labels = sorted(y_all.unique().tolist())
+    unique_labels = (
+        9 if len(unique_labels) > 8 else unique_labels
+    )  # sanity cap for printing
     no_activity_mask = y_all == 0
 
     print(
